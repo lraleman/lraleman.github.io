21580f399a38620e10d3b5c739eca2a6f65625b2
lraleman <Lucas.aleman@ou.edu>
2024-11-22 11:10:51 -0600

    11.22.24

--- HW9/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HW9.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
